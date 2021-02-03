eb9a3ecc2ab6d87843b8edcf0dba744d576dba48
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 17:45:52 -0600

    import artists router

--- api/api.js
+++ api/api.js
@@ -1,8 +1,14 @@
 // Import node modules
 const express = require('express');
 
+// Import artists router
+const artistsRouter = require('./artists');
+
 // setup api router
 const apiRouter = express.Router();
+
+// Setup artists route
+apiRouter.use('/artists', artistsRouter);
 
 // Export router
 module.exports = apiRouter;
