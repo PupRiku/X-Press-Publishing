1df46a9c7cbd31af37293f3d88c657211ff136ab
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 13:36:23 -0600

    import api and setup app to use on /api

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+// Node Module Imports
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
 const morgan = require('morgan');
 const express = require('express');
 
-const app = express();
+// API Router Imports
+const apiRouter = require("./api/api");
 
+// App setup
+const app = express();
 const PORT = process.env.PORT || 4000;
 
+// App doc config
 app.use(bodyParser.json());
 app.use(errorHandler());
 app.use(cors());
-
 app.use(morgan('dev'));
 
+// Routes setup
+app.use('/api', apiRouter);
+
+// Start App
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
 
+// Export server
 module.exports = app;
