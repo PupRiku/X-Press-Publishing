395331504e9246228578d83e8c76bf3b13c9c893
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 14:02:39 -0600

    Create artist table

--- migration.js
+++ migration.js
@@ -3,3 +3,13 @@ const sqlite3 = require("sqlite3");
 
 // Create new sqlite database
 const db = new sqlite3.Database('./database.sqlite');
+
+// Create artist table
+db.run(`CREATE TABLE IF NOT EXISTS 'Artist' (
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    biography TEXT NOT NULL,
+    is_currently_employed INTEGER NOT NULL DEFAULT 1,
+    PRIMARY KEY(id)
+)`);
