975721a91ea29036574367b5094a5ef4ad91fbfa
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 20:15:32 -0600

    Create series table

--- migration.js
+++ migration.js
@@ -13,3 +13,11 @@ db.run(`CREATE TABLE IF NOT EXISTS Artist (
     is_currently_employed INTEGER NOT NULL DEFAULT 1,
     PRIMARY KEY(id)
 )`);
+
+// Create series table
+db.run(`CREATE TABLE IF NOT EXISTS Series (
+    id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY(id)
+)`);
