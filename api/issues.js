73d8c72f997f501a7293f258fe22c156a1e4e7fe
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 22:18:59 -0600

    ALL THE BUG FIXING

--- api/issues.js
+++ api/issues.js
@@ -1,23 +1,59 @@
-// Import node modules
 const express = require('express');
-const sqlite3 = require('sqlite3');
-
-// setup api router
 const issuesRouter = express.Router({mergeParams: true});
 
-// setup database
+const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 // GET all issues of series
 issuesRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Issue WHERE series_id = $seriesId', {
-        $seriesId: req.params.seriesId
-    }, (err, issues) => {
-        if (err) {
-            next(err);
-        } else {
-            res.status(200).json({issues: issues});
+    const sql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+    const values = { $seriesId: req.params.seriesId};
+    db.all(sql, values, (error, issues) => {
+      if (error) {
+        next(error);
+      } else {
+        res.status(200).json({issues: issues});
+      }
+    });
+});
+
+// POST issue to series
+issuesRouter.post('/', (req, res, next) => {
+    const name = req.body.issue.name;
+    const issueNumber = req.body.issue.issueNumber;
+    const publicationDate = req.body.issue.publicationDate;
+    const artistId = req.body.issue.artistId;
+    const artistSql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+    const artistValues = {$artistId: artistId};
+    db.get(artistSql, artistValues, (error, artist) => {
+      if (error) {
+        next(error);
+      } else {
+        if (!name || !issueNumber || !publicationDate || !artist) {
+          return res.sendStatus(400);
         }
+  
+        const sql = 'INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id)' +
+            'VALUES ($name, $issueNumber, $publicationDate, $artistId, $seriesId)';
+        const values = {
+          $name: name,
+          $issueNumber: issueNumber,
+          $publicationDate: publicationDate,
+          $artistId: artistId,
+          $seriesId: req.params.seriesId
+        };
+  
+        db.run(sql, values, function(error) {
+          if (error) {
+            next(error);
+          } else {
+            db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`,
+              (error, issue) => {
+                res.status(201).json({issue: issue});
+              });
+          }
+        });
+      }
     });
 });
 
--- api/series.js
+++ api/series.js
@@ -1,105 +1,94 @@
-// Import node modules
 const express = require('express');
-
-// setup api router
 const seriesRouter = express.Router();
 
-// Import sqlite3
 const sqlite3 = require('sqlite3');
-
-// setup database
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-// Import issues router
-const issuesRouter = require('./issues');
+const issuesRouter = require('./issues.js');
 
-// Setup router param
 seriesRouter.param('seriesId', (req, res, next, seriesId) => {
-    db.get("SELECT * FROM Series WHERE id = $seriesId", {
-        $seriesId: seriesId
-    }, (err, series) => {
-        if (err) {
-            next(err);
-        } else if (series) {
-            req.series = series;
-            next();
-        } else {
-            res.sendStatus(404);
-        }
-    });
+  const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
+  const values = {$seriesId: seriesId};
+  db.get(sql, values, (error, series) => {
+    if (error) {
+      next(error);
+    } else if (series) {
+      req.series = series;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
 
-// Mount issues router
-seriesRouter.use('/:seriedId/issues', issuesRouter);
+seriesRouter.use('/:seriesId/issues', issuesRouter);
 
-// GET all series
 seriesRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Series', (err, series) => {
-        if (err) {
-            next(err);
-        } else {
-            res.status(200).json({series: series});
-        }
-    });
+  db.all('SELECT * FROM Series', (err, series) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({series: series});
+    }
+  });
 });
 
-// GET series by id
 seriesRouter.get('/:seriesId', (req, res, next) => {
-    res.status(200).send({series: req.series});
+  res.status(200).json({series: req.series});
 });
 
-// POST new series to DB
 seriesRouter.post('/', (req, res, next) => {
-    const name = req.body.series.name;
-    const description = req.body.series.description;
-    if(!name || !description) {
-        return res.sendStatus(400);
-    }
+  const name = req.body.series.name,
+        description = req.body.series.description;
+  if (!name || !description) {
+    return res.sendStatus(400);
+  }
 
-    db.run('INSERT INTO Series (name, description) VALUES ($name, $description)', {
-        $name: name,
-        $description: description
-    }, function(err) {
-        if (err) {
-            next(err);
-        } else {
-            db.get(`SELECT * FROM Series WHERE id=${this.lastID}`, (error, series) => {
-                if (error) {
-                    next (error);
-                } else {
-                    res.status(201).json({series: series});
-                }
-            })
-        }
-    });
+  const sql = 'INSERT INTO Series (name, description) VALUES ($name, $description)';
+  const values = {
+    $name: name,
+    $description: description
+  };
+
+  db.run(sql, values, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Series WHERE Series.id = ${this.lastID}`,
+        (error, series) => {
+          res.status(201).json({series: series});
+        });
+    }
+  });
 });
 
-// PUT an update to a series by id
 seriesRouter.put('/:seriesId', (req, res, next) => {
-    const name = req.body.series.name;
-    const description = req.body.series.description;
-    if(!name || !description) {
-        return res.sendStatus(400);
-    }
+  const name = req.body.series.name,
+        description = req.body.series.description;
+  if (!name || !description) {
+    return res.sendStatus(400);
+  }
 
-    db.run('UPDATE Series SET name = $name, description = $description WHERE id = $seriesId', {
-        $name: name,
-        $description: description,
-        $seriesId: req.params.seriesId
-    }, err => {
-        if (err) {
-            next(err);
-        } else {
-            db.get(`SELECT * FROM Series WHERE id = ${req.params.seriesId}`, (error, series) => {
-                if (error) {
-                    next(error);
-                } else {
-                    res.status(200).json({series: series});
-                }
-            })
-        }
-    });
+  const sql = 'UPDATE Series SET name = $name, description = $description ' +
+      'WHERE Series.id = $seriesId';
+  const values = {
+    $name: name,
+    $description: description,
+    $seriesId: req.params.seriesId
+  };
+
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Series WHERE Series.id = ${req.params.seriesId}`,
+        (error, series) => {
+          res.status(200).json({series: series});
+        });
+    }
+  });
 });
+
 
 // Export router
 module.exports = seriesRouter;
