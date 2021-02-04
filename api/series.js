9bbdbf3f76cd4d77090649cb5037b2a85319093e
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 20:39:24 -0600

    GET all series

--- api/series.js
+++ api/series.js
@@ -8,5 +8,16 @@ const seriesRouter = express.Router();
 // setup database
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// GET all series
+seriesRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Series', (err, series) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({series: series});
+        }
+    });
+});
+
 // Export router
 module.exports = seriesRouter;
