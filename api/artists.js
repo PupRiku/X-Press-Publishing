bf01fdc78e9d4960a695ef94f9e5faed1567c931
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 19:11:35 -0600

    send responses

--- api/artists.js
+++ api/artists.js
@@ -10,7 +10,11 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 artistsRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, rows) => {
-        
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({artists: rows});
+        }
     });
 });
 
