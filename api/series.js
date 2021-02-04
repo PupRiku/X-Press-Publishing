f43ba212843f396a3fc764123949a1181100ef1e
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-03 20:25:46 -0600

    setup db

--- api/series.js
+++ api/series.js
@@ -1,8 +1,12 @@
 // Import node modules
 const express = require('express');
+const sqlite3 = require('sqlite3');
 
 // setup api router
 const seriesRouter = express.Router();
+
+// setup database
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 // Export router
 module.exports = seriesRouter;
